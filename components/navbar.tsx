b7fe9bed76e10ca9081d978d9ab1ea5ffcf5f979
Leonid Lobanov <leonid.lobanov@pixster.eu>
2024-04-28 19:16:19 +0300

    Theme toggler added.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,9 @@
-
 import { UserButton, auth } from "@clerk/nextjs";
 import MainNav from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -20,9 +20,10 @@ const Navbar = async () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <StoreSwitcher items={stores}/>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
